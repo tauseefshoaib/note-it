5adb4f87bab3f700589f8e60822fff83d893fa26
tauseefshoaib <tauseefshoaib@gmail.com>
2021-12-26 01:30:08 +0530

    added github icon and hyperlink in about app

--- lib/about.dart
+++ lib/about.dart
@@ -1,14 +1,41 @@
-// ignore_for_file: unnecessary_const
+// ignore_for_file: unnecessary_const, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutApp extends StatelessWidget {
   const AboutApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: const Center(child: const Text('Created with ❤ ️by Tauseef')),
-    );
+    return Scaffold(
+        body: SafeArea(
+      child: Center(
+        child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Created withText ❤ ️by Tauseef'),
+            SizedBox(
+              height: 15,
+            ),
+            InkWell(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(FontAwesomeIcons.github),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text('tauseefshoaib'),
+                ],
+              ),
+              onTap: () => launch('https://github.com/tauseefshoaib'),
+            )
+          ],
+        ),
+      ),
+    ));
   }
 }
