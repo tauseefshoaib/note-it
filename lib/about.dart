f5a93416ac2cb2d4ec5423b38e2d66f5f25d335a
Tauseef Shoaib <tauseefshoaib@gmail.com>
2022-12-06 18:27:45 +0530

    checking commit

--- lib/about.dart
+++ lib/about.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+// heyyyyyyyyyyyyyy
 class AboutApp extends StatelessWidget {
   const AboutApp({Key? key}) : super(key: key);
 
